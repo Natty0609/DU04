6d0d2ca93388df2fc33dd58e2892a1cbf9b7ff2c
Nataliya Tahova <ntahova@gmail.com>
2024-03-06 13:59:39 +0100

    oprava kodu

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ const max2 = (a, b, c) => {
         return a + ' ' + "je větší"
     } else if (b > a && b > c) {
 
-        return b + ' ' +
-            "je větší"
+        return b + ' ' + "je větší"
     } else if (c > a && c > b) {
 
         return c + ' ' + "je větší"
